21faf1ceedaa511e63a54ea43f39fa1db391e3f5
Mingqi Shao <minsha@microsoft.com>
2021-03-09 16:17:57 -0800

    finish External Support

--- ContosoAdsCloudService/Package.ps1
+++ ContosoAdsCloudService/Package.ps1
@@ -25,7 +25,7 @@ if ($LASTEXITCODE -ne 0) {
 }
 
 # prepare classic Ev2
-Copy-Item -Path $PSScriptRoot\ServiceConfiguration.Cloud.cscfg -Destination $classicOutDir\ServiceConfigurationf.cscfg
+Copy-Item -Path $PSScriptRoot\ServiceConfiguration.Cloud.cscfg -Destination $classicOutDir\ServiceConfiguration.cscfg
 Get-ChildItem -Path $PSScriptRoot\ev2\classic | ForEach-Object {
     $content = Get-Content -raw -Path $_.FullName
     $classicEv2DeploymentVariables.GetEnumerator() | ForEach-Object {
@@ -34,7 +34,25 @@ Get-ChildItem -Path $PSScriptRoot\ev2\classic | ForEach-Object {
     }
     Set-Content -Value $content -Path (Join-Path -Path $classicOutDir -ChildPath $_.Name)
 }
-
 Set-Content -Value "1.0.0.0" -Path (Join-Path -Path $classicOutDir -ChildPath "buildver.txt")
 
 # external support Ev2
+$classicEv2DeploymentVariables = @{
+    SubscriptionId = "da28f5e5-aa45-46fe-90c8-053ca49ab4b5"
+    ResourceGroupName = "minsha-az-prototype-externalsupport"
+    DeploymentLabel = ( Get-Date -Format "yyyy-MM-dd HH:mm")
+    CloudServiceName = "minsha-az-prototype-externalsupport"
+}
+$externalSupportOutDir = "$PSScriptRoot\bin\externalSupport\"
+New-Item -path $externalSupportOutDir -ItemType Directory -Force
+Copy-Item -Path $PSScriptRoot\ServiceConfiguration.ExternalSupport.cscfg -Destination $externalSupportOutDir\ContosoAdsCloudService.cscfg
+Copy-Item -Path $classicOutDir\ContosoAdsCloudService.cspkg -Destination $externalSupportOutDir\ContosoAdsCloudService.cspkg
+Get-ChildItem -Path $PSScriptRoot\ev2\externalSupport | ForEach-Object {
+    $content = Get-Content -raw -Path $_.FullName
+    $classicEv2DeploymentVariables.GetEnumerator() | ForEach-Object {
+        $placeholder = "<"+$_.key+">"
+        $content = $content -replace $placeholder,$_.Value
+    }
+    Set-Content -Value $content -Path (Join-Path -Path $externalSupportOutDir -ChildPath $_.Name)
+}
+Set-Content -Value "1.0.0.0" -Path (Join-Path -Path $externalSupportOutDir -ChildPath "buildver.txt")
